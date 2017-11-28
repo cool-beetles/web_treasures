ddfe2ab0bf23c5c1f181b8215cd250e0fef8b83c
Ada <adrianna.kucinska@onet.eu>
2017-11-28 11:23:50 +0100

    Create Treasure model

---
+++ app/models/treasure.rb
@@ -0,0 +1,2 @@
+class Treasure < ApplicationRecord
+end
---
+++ db/migrate/20171128102235_create_treasures.rb
@@ -0,0 +1,15 @@
+class CreateTreasures < ActiveRecord::Migration[5.1]
+  def change
+    create_table :treasures do |t|
+      t.integer :owner_id
+      t.integer :type_id
+      t.integer :storage_id
+      t.string :trashed
+      t.string :title
+      t.string :description
+      t.string :special_note
+
+      t.timestamps
+    end
+  end
+end
