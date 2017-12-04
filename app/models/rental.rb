0d13d7079933d93c6769588485e0c3d35b383817
Ada <adrianna.kucinska@onet.eu>
2017-12-04 12:59:19 +0100

    #12 Add rental validation

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,5 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :treasure
+  validates_presence_of :user, :treasure
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RentalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save treasure without owner, type or title" do
+    user = User.new(first_name: "John", last_name: "Snow")
+    type = Type.new(name: "Toys")
+    treasure = Treasure.new(owner: user.id, type: type.id, title: "Darts")
+
+    rental = Rental.create(user: user.id, treasure: treasure.id)
+    assert_not rental.valid?
+
+    rental = Rental.create(user: user.id)
+    assert_not rental.valid?
+
+    rental = Rental.create(treasure: treasure.id)
+    assert_not rental.valid?
+  end
 end
