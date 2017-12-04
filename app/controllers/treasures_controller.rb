e463e75d55f7209fbc143d137025ca2d7c3f2d38
Ada <adrianna.kucinska@onet.eu>
2017-12-04 19:42:00 +0100

    #20 Create treasure view

--- app/controllers/treasures_controller.rb
+++ app/controllers/treasures_controller.rb
@@ -2,4 +2,8 @@ class TreasuresController < ActionController::Base
   def index
     @treasures = Treasure.all
   end
+
+  def show
+    @treasure = Treasure.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'treasures/index'
+  get 'treasures/show'
 
   resources :treasures
 
-  root 'treasures#index'
+  root 'treasures#index/:id'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
