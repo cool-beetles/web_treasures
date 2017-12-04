fd1c00f4a652c69c86bf42c32d9e039e78c88531
Ada <adrianna.kucinska@onet.eu>
2017-12-04 13:32:17 +0100

    #15 Add address validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
   has_one :storage
   has_one :user
+  validates_presence_of :street, :city
+  validates :zip_code, length: { maximum: 7 }
 end
--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save address with too long zip_code or without street and city" do 
+    address = Address.create(street: "Wilczynskiego", city: "Olsztyn", zip_code: "10-900")
+    assert address.valid?
+
+    address = Address.create(city: "Olsztyn", zip_code: "10-900")
+    assert_not address.valid?
+
+    address = Address.create(street: "Wilczynskiego", zip_code: "10-900")
+    assert_not address.valid?
+
+    address = Address.create(street: "Wilczynskiego", city: "Olsztyn", zip_code: "100-9000")
+    assert_not address.valid?
+  end
 end
