fca2f54f11bb391a707a44ab4b8abb34b94eea0b
Ada <adrianna.kucinska@onet.eu>
2017-12-04 12:50:28 +0100

    #11 Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :treasures
   has_many :rentals
-  belongs_to :address
+  belongs_to :address, optional: true
+  validates :first_name, length: { minimum: 3 }
+  validates :last_name, length: { minimum: 3 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user with to short first_name and last_name or without them" do 
+  user = User.new(first_name: "John", last_name: "Snow")
+  assert_not user.invalid?
+  
+  user = User.new(first_name: "Jo", last_name: "Snow")
+  assert_not user.valid?
+  
+  user = User.new(first_name: "John", last_name: "Sn")
+  assert_not user.valid?
+
+  user = User.new(last_name: "Snow")
+  assert_not user.valid?
+
+  user = User.new(first_name: "John")
+  assert_not user.valid?
+  end
 end
