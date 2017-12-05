b82c1b7e71d09d9282ef7f4077e80dfd1d70ade0
Ada <adrianna.kucinska@onet.eu>
2017-12-05 18:49:33 +0100

    #11 Make one test = one assert for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,20 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user with too short first_name and last_name or without them" do 
+  test "should save user with proper first_name and last_name" do 
     user = User.new(first_name: "John", last_name: "Snow")
-    assert_not user.invalid?
-    
+    assert user.valid?
+  end
+
+  test "should not save user with first name shorter than 3 letters" do  
     user = User.new(first_name: "Jo", last_name: "Snow")
     assert_not user.valid?
-    
+    assert_equal [:first_name], user.errors.keys
+  end 
+
+  test "should not save user with last name shorter than 3 letters" do  
     user = User.new(first_name: "John", last_name: "Sn")
     assert_not user.valid?
+    assert_equal [:last_name], user.errors.keys
+  end
 
+  test "should not save user without first name" do
     user = User.new(last_name: "Snow")
     assert_not user.valid?
+    assert_equal [:first_name], user.errors.keys
+  end
 
+  test "should not save user without last name" do
     user = User.new(first_name: "John")
     assert_not user.valid?
+    assert_equal [:last_name], user.errors.keys
   end
 end
