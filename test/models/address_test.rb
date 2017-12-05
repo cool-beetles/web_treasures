dce545cca91cf49d000a6776fb11cfc7803072f4
Ada <adrianna.kucinska@onet.eu>
2017-12-05 20:40:03 +0100

    #15 Add fixtures for address validations

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
   test "should save address with proper zip_code street and city" do 
-    address = Address.create(street: "Wilczynskiego", city: "Olsztyn", zip_code: "10-900")
+    address = Address.create(street: addresses(:my_address).street, city: addresses(:my_address).city, zip_code: addresses(:my_address).zip_code)
     assert address.valid?
   end
 
   test "should not save address without street" do 
-    address = Address.create(city: "Olsztyn", zip_code: "10-900")
+    address = Address.create(city: addresses(:my_address).city, zip_code: addresses(:my_address).zip_code)
     assert_not address.valid?
     assert_equal [:street], address.errors.keys
   end
 
   test "should not save address without city" do 
-    address = Address.create(street: "Wilczynskiego", zip_code: "10-900")
+    address = Address.create(street: addresses(:my_address).street, zip_code: addresses(:my_address).zip_code)
     assert_not address.valid?
     assert_equal [:city], address.errors.keys
   end
   
   test "should not save address with zip_code longer than 7 characters" do 
-    address = Address.create(street: "Wilczynskiego", city: "Olsztyn", zip_code: "100-9000")
+    address = Address.create(street: addresses(:address_for_test).street, city: addresses(:address_for_test).city, zip_code: addresses(:address_for_test).zip_code)
     assert_not address.valid?
     assert_equal [:zip_code], address.errors.keys
   end
