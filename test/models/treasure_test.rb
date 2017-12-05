64f970a4595a518cd8e52f01fc48f86137663941
Ada <adrianna.kucinska@onet.eu>
2017-12-05 20:04:40 +0100

    #10 Add fixtures for treasure validations

--- test/models/treasure_test.rb
+++ test/models/treasure_test.rb
@@ -1,28 +1,26 @@
 require 'test_helper'
 
 class TreasureTest < ActiveSupport::TestCase
-  owner = User.new(first_name: "Ada", last_name: "Kucinska")
-  type = Type.new(name: "Toys")
 
   test "should save treasure with owner, type or title" do 
-    treasure = Treasure.create(owner: owner, type: type, title: "Barbie")
+    treasure = Treasure.create(owner: users(:owner), type: types(:toy), title: "Barbie")
     assert treasure.valid?
   end
 
   test "should not save treasure without owner" do 
-    treasure = Treasure.create(type: type, title: "Barbie")
+    treasure = Treasure.create(type: types(:toy), title: "Barbie")
     assert_not treasure.valid?
     assert_equal [:owner], treasure.errors.keys
   end
 
   test "should not save treasure without title" do 
-    treasure = Treasure.create(owner: owner, type: type)
+    treasure = Treasure.create(owner: users(:owner), type: types(:toy))
     assert_not treasure.valid?
     assert_equal [:title], treasure.errors.keys
   end
 
   test "should not save treasure without type" do 
-    treasure = Treasure.create(owner: owner, title: "Barbie")
+    treasure = Treasure.create(owner: users(:owner), title: "Barbie")
     assert_not treasure.valid?
     assert_equal [:type], treasure.errors.keys
   end
