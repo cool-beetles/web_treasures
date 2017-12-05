c6ef5a61b5e26576ad217fb8b02bf7040ab9e63d
Ada <adrianna.kucinska@onet.eu>
2017-12-05 18:38:41 +0100

    #10 Make one test = one assert and fix fixtures files

--- test/models/treasure_test.rb
+++ test/models/treasure_test.rb
@@ -1,20 +1,29 @@
 require 'test_helper'
 
 class TreasureTest < ActiveSupport::TestCase
-  test "should not save treasure without owner, type or title" do 
-    owner = User.new(first_name: "Ada", last_name: "Kucinska")
-    type = Type.new(name: "Toys")
-    
-    treasure = Treasure.create(owner: owner.id, type: type.id, title: "Barbie")
-    assert_not treasure.valid?
+  owner = User.new(first_name: "Ada", last_name: "Kucinska")
+  type = Type.new(name: "Toys")
 
-    treasure = Treasure.create(type: type.id, title: "Barbie")
-    assert_not treasure.valid?
+  test "should save treasure with owner, type or title" do 
+    treasure = Treasure.create(owner: owner, type: type, title: "Barbie")
+    assert treasure.valid?
+  end
 
-    treasure = Treasure.create(owner: owner.id, type: type.id)
+  test "should not save treasure without owner" do 
+    treasure = Treasure.create(type: type, title: "Barbie")
     assert_not treasure.valid?
+    assert_equal [:owner], treasure.errors.keys
+  end
 
-    treasure = Treasure.create(owner: owner.id, title: "Barbie")
+  test "should not save treasure without title" do 
+    treasure = Treasure.create(owner: owner, type: type)
     assert_not treasure.valid?
+    assert_equal [:title], treasure.errors.keys
+  end
+
+  test "should not save treasure without type" do 
+    treasure = Treasure.create(owner: owner, title: "Barbie")
+    assert_not treasure.valid?
+    assert_equal [:type], treasure.errors.keys
   end
 end
