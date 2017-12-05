e4a6a36e678eccceb1d19ccfeb6b4cb2f20d48b2
Ada <adrianna.kucinska@onet.eu>
2017-12-05 20:36:04 +0100

    #14 Add fixtures for storage validations

--- test/models/storage_test.rb
+++ test/models/storage_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StorageTest < ActiveSupport::TestCase
   test "should save storage with proper name" do 
-    storage = Storage.new(name: "My house")
+    storage = Storage.new(name: storages(:house).name)
     assert storage.valid?
   end
 
   test "should not save storage with name longer than 15 letters" do  
-    storage = Storage.new(name: "House of my friend Emily")
+    storage = Storage.new(name: storages(:house_of_friend).name)
     assert_not storage.valid?
     assert_equal [:name], storage.errors.keys
   end
