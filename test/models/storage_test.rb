e27cb97da6e7243a4bb2db0f0598eaa9862fcf67
Ada <adrianna.kucinska@onet.eu>
2017-12-05 19:03:06 +0100

    #14 Make one test = one assert for storage

--- test/models/storage_test.rb
+++ test/models/storage_test.rb
@@ -1,11 +1,14 @@
 require 'test_helper'
 
 class StorageTest < ActiveSupport::TestCase
-  test "should not save storage with too long name" do 
+  test "should save storage with proper name" do 
     storage = Storage.new(name: "My house")
-    assert_not storage.invalid?
+    assert storage.valid?
+  end
 
+  test "should not save storage with name longer than 15 letters" do  
     storage = Storage.new(name: "House of my friend Emily")
     assert_not storage.valid?
+    assert_equal [:name], storage.errors.keys
   end
 end
