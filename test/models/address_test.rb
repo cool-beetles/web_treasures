099f0291a92a437c4522f317e3a175b826ff9c94
Ada <adrianna.kucinska@onet.eu>
2017-12-05 19:08:36 +0100

    #15 Make one test = one assert for address

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,17 +1,26 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  test "should not save address with too long zip_code or without street and city" do 
+  test "should save address with proper zip_code street and city" do 
     address = Address.create(street: "Wilczynskiego", city: "Olsztyn", zip_code: "10-900")
     assert address.valid?
+  end
 
+  test "should not save address without street" do 
     address = Address.create(city: "Olsztyn", zip_code: "10-900")
     assert_not address.valid?
+    assert_equal [:street], address.errors.keys
+  end
 
+  test "should not save address without city" do 
     address = Address.create(street: "Wilczynskiego", zip_code: "10-900")
     assert_not address.valid?
-
+    assert_equal [:city], address.errors.keys
+  end
+  
+  test "should not save address with zip_code longer than 7 characters" do 
     address = Address.create(street: "Wilczynskiego", city: "Olsztyn", zip_code: "100-9000")
     assert_not address.valid?
+    assert_equal [:zip_code], address.errors.keys
   end
 end
