e1761e42e9a7f0448d963ad23e6ee734fb29cf4a
Ada <adrianna.kucinska@onet.eu>
2017-12-06 15:54:02 +0100

    #15 Add fixtures to address according to the comments

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -2,24 +2,26 @@ require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
   test "should save address with proper zip_code street and city" do 
-    address = Address.new(street: addresses(:my_address).street, city: addresses(:my_address).city, zip_code: addresses(:my_address).zip_code)
+    address = addresses(:my_address)
     assert address.valid?
   end
 
   test "should not save address without street" do 
-    address = Address.new(city: addresses(:my_address).city, zip_code: addresses(:my_address).zip_code)
+    address = addresses(:my_address)
+    address.street = nil
     assert_not address.valid?
     assert_equal [:street], address.errors.keys
   end
 
   test "should not save address without city" do 
-    address = Address.new(street: addresses(:my_address).street, zip_code: addresses(:my_address).zip_code)
+    address = addresses(:my_address)
+    address.city = nil
     assert_not address.valid?
     assert_equal [:city], address.errors.keys
   end
   
   test "should not save address with zip_code longer than 7 characters" do 
-    address = Address.new(street: addresses(:address_for_test).street, city: addresses(:address_for_test).city, zip_code: addresses(:address_for_test).zip_code)
+    address = Address.new(street: "Wilczynskiego", city: "Olsztyn", zip_code: "110-1101")
     assert_not address.valid?
     assert_equal [:zip_code], address.errors.keys
   end
