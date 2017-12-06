4d0bcccfd0c662333000fdca74d2702426a602b8
Ada <adrianna.kucinska@onet.eu>
2017-12-06 15:43:39 +0100

    #10 Add fixtures to treasure according to the comments

--- test/models/treasure_test.rb
+++ test/models/treasure_test.rb
@@ -2,24 +2,27 @@ require 'test_helper'
 
 class TreasureTest < ActiveSupport::TestCase
   test "should save treasure with owner, type or title" do 
-    treasure = Treasure.new(owner: users(:owner), type: types(:toy), title: "Barbie")
+    treasure = treasures(:doll)
     assert treasure.valid?
   end
 
   test "should not save treasure without owner" do 
-    treasure = Treasure.new(type: types(:toy), title: "Barbie")
+    treasure = treasures(:doll)
+    treasure.owner = nil
     assert_not treasure.valid?
     assert_equal [:owner], treasure.errors.keys
   end
 
   test "should not save treasure without title" do 
-    treasure = Treasure.new(owner: users(:owner), type: types(:toy))
+    treasure = treasures(:doll)
+    treasure.title = nil
     assert_not treasure.valid?
     assert_equal [:title], treasure.errors.keys
   end
 
   test "should not save treasure without type" do 
-    treasure = Treasure.new(owner: users(:owner), title: "Barbie")
+    treasure = treasures(:doll)
+    treasure.type = nil
     assert_not treasure.valid?
     assert_equal [:type], treasure.errors.keys
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,30 +2,30 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "should save user with proper first_name and last_name" do 
-    user = User.new(first_name: users(:owner).first_name, last_name: users(:owner).last_name)
+    user = users(:owner)
     assert user.valid?
   end
 
   test "should not save user with first name shorter than 3 letters" do 
-    user = User.new(first_name: users(:too_short_first_name).first_name, last_name: users(:too_short_first_name).last_name)
+    user = User.new(first_name: "Jo", last_name: "Snow")
     assert_not user.valid?
     assert_equal [:first_name], user.errors.keys
   end 
 
   test "should not save user with last name shorter than 3 letters" do  
-    user = User.new(first_name: users(:too_short_last_name).first_name , last_name: users(:too_short_last_name).last_name)
+    user = User.new(first_name: "John", last_name: "Sn")
     assert_not user.valid?
     assert_equal [:last_name], user.errors.keys
   end
 
   test "should not save user without first name" do
-    user = User.new(last_name: users(:friend).last_name)
+    user = User.new(last_name: users(:friend))
     assert_not user.valid?
     assert_equal [:first_name], user.errors.keys
   end
 
   test "should not save user without last name" do
-    user = User.new(first_name: users(:owner).first_name)
+    user = User.new(first_name: users(:owner))
     assert_not user.valid?
     assert_equal [:last_name], user.errors.keys
   end
