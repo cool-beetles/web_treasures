5471ffab4aaba1dbac0e8f3a39ea07334f717df7
Ada <adrianna.kucinska@onet.eu>
2017-12-06 15:49:14 +0100

    #13 Add fixtures to rental according to the comments

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -2,18 +2,20 @@ require 'test_helper'
 
 class RentalTest < ActiveSupport::TestCase
   test "should save rental with user and treasure" do
-    rental = Rental.new(user: users(:owner), treasure: treasures(:doll))
+    rental = rentals(:first_rental)
     assert rental.valid?
   end
 
   test "should not save rental without treasure" do
-    rental = Rental.new(user: users(:friend))
+    rental = rentals(:first_rental)
+    rental.treasure = nil
     assert_not rental.valid?
     assert_equal [:treasure], rental.errors.keys
   end
 
   test "should not save rental without user" do
-    rental = Rental.new(treasure: treasures(:puzzle))
+    rental = rentals(:first_rental)
+    rental.user = nil
     assert_not rental.valid?
     assert_equal [:user], rental.errors.keys
   end
