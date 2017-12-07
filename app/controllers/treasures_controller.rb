20892fe055f2210b022462456116a32f360950cd
Ada <adrianna.kucinska@onet.eu>
2017-12-07 11:28:54 +0100

    Add edit and update actions for treasure

--- app/controllers/treasures_controller.rb
+++ app/controllers/treasures_controller.rb
@@ -21,8 +21,22 @@ class TreasuresController < ActionController::Base
     end
   end
 
+  def edit
+    @treasure = Treasure.find(params[:id])
+    render action: 'edit'
+  end
+
+  def update
+    @treasure = Treasure.find(params[:id])
+    if @treasure.update(params_treasure)
+      redirect_to @treasure
+    else
+      render :edit
+    end
+  end
+
   private 
-  
+
   def params_treasure
     params.require(:treasure).permit(:owner_id, :type_id, :title, :description, :special_note, :storage_id, :trashed)
   end
