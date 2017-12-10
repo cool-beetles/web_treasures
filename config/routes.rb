03a577002ef02dc3762834f4ef6499270776d81b
Ada <adrianna.kucinska@onet.eu>
2017-12-10 23:45:56 +0100

    #34 Add Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   resources :treasures
   resources :storages
+  resources :welcome
 
-  root 'treasures#index'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
