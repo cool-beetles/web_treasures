c86c799e3e2668997884230ab244a64143237914
Ada <adrianna.kucinska@onet.eu>
2017-12-10 17:54:17 +0100

    #28 Add foreign key to Storage model

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,5 +1,5 @@
 class Storage < ApplicationRecord
-  has_many :treasures
+  has_many :treasures, foreign_key: 'owner_id'
   belongs_to :address, optional: true
   validates :name, length: { maximum: 15 }
 end
