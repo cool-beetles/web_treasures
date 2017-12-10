70592cd76dfad371f04a9469a497f74003407b1f
Ada <adrianna.kucinska@onet.eu>
2017-12-10 21:25:16 +0100

    #28 Fix tests

--- test/models/storage_test.rb
+++ test/models/storage_test.rb
@@ -2,16 +2,22 @@ require 'test_helper'
 
 class StorageTest < ActiveSupport::TestCase
   def setup
-  @storage = storages(:house)
+    @storage = storages(:house)
   end
   
   test "should save storage with proper name" do 
     assert @storage.valid?
   end
 
-  test "should not save storage with name longer than 15 letters" do  
-    storage = Storage.new(name: "House of my friend Emily")
-    assert_not storage.valid?
-    assert_equal [:name], storage.errors.keys
+  test "should not save storage with name longer than 15 letters" do
+    @storage.name = "House of my friend Emily"
+    assert_not @storage.valid?
+    assert_equal [:name], @storage.errors.keys
+  end
+
+  test "should not save storage without owner" do
+    @storage.owner = nil
+    assert_not @storage.valid?
+    assert_equal [:owner], @storage.errors.keys
   end
 end
