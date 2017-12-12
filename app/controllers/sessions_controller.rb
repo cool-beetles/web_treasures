d4fdfa8d59e1dd79847e5ff503aa3742076b94ef
Ada <adrianna.kucinska@onet.eu>
2017-12-12 21:07:04 +0100

    #38 Add password hashing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @account = Account.find_by(email: params[:session][:email]) 
-
-    if @account != nil && params[:session][:password_digest] == @account.password_digest
+   if @account = Account.authenticate(params[:session][:email], params[:session][:password_digest])
       session[:user_id] = @account.user_id
       flash[:success] = 'You are log in!'
       redirect_to treasures_path
-    else      
+    else
       flash[:alert] =  'You should have an account'
       redirect_to root_path
-    end 
+    end
   end
 
   def destroy
-    @account = Account.find_by(user_id: session[:user_id])
     session.destroy
     flash[:success] = 'You are log out!'
     redirect_to root_path
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,13 @@
 class Account < ApplicationRecord
   has_secure_password
   belongs_to :user
+
+  def self.authenticate(email, password)
+    @account = Account.find_by(email: email) 
+    if @account != nil && @account.authenticate(password)
+      @account
+    else
+      nil
+    end
+  end
 end
