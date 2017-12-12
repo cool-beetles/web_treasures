0191d44d74cc41f31de7231124fef45970e3eb9a
Ada <adrianna.kucinska@onet.eu>
2017-12-12 12:24:43 +0100

    #36 Add flash messages in apllication layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,10 @@ class SessionsController < ApplicationController
     if @account == Account.find_by(password: params[:session][:password])
       session[:user_id] = @account.user_id
       @account.save
+      flash[:success] = 'You are log in!'
       redirect_to treasures_path
     else
+      flash[:alert] =  'You should have an account'
       redirect_to root_path
     end 
   end
