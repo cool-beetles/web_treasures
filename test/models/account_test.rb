a9acddc816cf63c3b59128ac1858f5259a7a7d79
Ada <adrianna.kucinska@onet.eu>
2017-12-12 22:44:51 +0100

    #38 add tests for Account.authenticate method

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @account = accounts(:owners_account)
+    @email = @account.email
+    @password = "secret123"
+  end
+
+  test "Should return @account if email and password are correct" do
+    result = Account.authenticate(@email, @password)
+    assert_equal @account, result
+  end
+
+  test "Should return nil if password is incorrect" do
+    @password = "secret"
+    result = (Account.authenticate(@email, @password))
+    assert_nil result
+  end
+
+  test "Should return nil if email is null" do
+    @email = nil
+    result = Account.authenticate(@email, @password)
+    assert_nil result
+  end
+
+  test "Should return nil if email doesn't exist in database" do
+    @email = "test@test.com"
+    result = Account.authenticate(@email, @password)
+    assert_nil result
+  end
 end
