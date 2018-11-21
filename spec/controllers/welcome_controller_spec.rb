5495816f91dbe1fd936de66fc1fd9306af443bd3
cool-beetles <adrianna.kucinska@onet.eu>
2018-11-21 21:39:08 +0100

    remove (response.body).to eq''

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe WelcomeController, type: :controller do
     it "renders the welcome/index template" do
       get :index
       expect(response).to render_template("welcome/index")
-      expect(response.body).to eq ""
     end
   end
 end
