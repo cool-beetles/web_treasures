26d424e43aa65cdf3a4a58a469b2ab2ec24c5b5b
cool-beetles <adrianna.kucinska@onet.eu>
2018-11-24 20:58:25 +0100

    #45 organize tests descriptions

--- spec/controllers/treasures_controller_spec.rb
+++ spec/controllers/treasures_controller_spec.rb
@@ -11,10 +11,28 @@ RSpec.describe TreasuresController, type: :controller do
       get :index
       expect(response).to render_template("treasures/index")
     end
+
+    it "render treasures#show template" do
+      get :show, params: { id: 1 }
+      expect(response.staus).to eq(200)
+    end
+
+    it "render treasures#new template" do
+    end
+    
+    context "create treasure with valid attributes" do
+      it "should save the new story in the database"
+      it "should redirect to the treasures#index page"
+    end    
+    
+    context "create treasure with invalid attributes" do
+      it "should not save the new story in the database"
+      it "should render treasures#new template"
+    end
   end
 
   describe "as guest user" do  
-    it "redirect to login template" do
+    it "redirect to login page" do
       get :index
       expect(response).to redirect_to(login_path) 
     end
