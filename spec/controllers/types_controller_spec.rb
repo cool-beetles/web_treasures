a4dab03ad8694df65a0ca27e111c8ba533c80a5f
cool-beetles <adrianna.kucinska@onet.eu>
2018-12-07 18:18:40 +0100

    #45 change describe for types test

--- spec/controllers/types_controller_spec.rb
+++ spec/controllers/types_controller_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
 RSpec.describe TypesController, type: :controller do
-  describe "index" do
+  describe "as logged user" do
       before do
       user = User.create(first_name:"test",last_name:"test")
       allow_any_instance_of(described_class).to receive(:current_user) {user}
     end
 
-    it "renders the types/index template" do
-      get :index
-      expect(response).to render_template("types/index")
+    describe "GET index" do
+      it "renders the types/index template" do
+        get :index
+        expect(response).to render_template("types/index")
+      end
     end
   end
 end
