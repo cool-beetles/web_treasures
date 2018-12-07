8eae440ca148be7bbfaa990479e63d1a7dd2b145
cool-beetles <adrianna.kucinska@onet.eu>
2018-12-07 17:22:51 +0100

    #45 Change test describe

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe WelcomeController, type: :controller do
-  describe "index" do
+  describe "GET index" do
     it "renders the welcome/index template" do
       get :index
       expect(response).to render_template("welcome/index")
